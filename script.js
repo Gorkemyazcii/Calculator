2b3997e9e02ae0c0669285b23a0de480123920bd
Görkem <Gkrem33@gmail.com>
2023-08-29 15:08:11 +0300

    Operator düzenleme

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
 let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitSecondValue = false;
 
 updateDisplay();
 
@@ -20,16 +23,21 @@ keys.addEventListener('click', function (e) {
     if (element.classList.contains('operator')) {
 
         console.log('operator', element.value);
+        handleOperator(element.value);
         return;
     }
 
     if (element.classList.contains('decimal')) {
         console.log('decimal', element.value);
+        inputDecimal(element.value);
+        updateDisplay();
         return;
     }
     if (element.classList.contains('allClear')) {
 
         console.log('allClear', element.value);
+        clear();
+        updateDisplay();
         return;
     }
     if (element.classList.contains('negatif')) {
@@ -44,4 +52,37 @@ keys.addEventListener('click', function (e) {
     }
 
     console.log('number', element.value);
-})
+    inputNumber(element.value);
+    updateDisplay();
+});
+
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+    if (firstValue === null) {
+        firstValue = value;
+    }
+    waitSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitSecondValue);
+}
+
+function inputNumber(num) {
+    if (waitSecondValue) {
+        displayValue = num;
+        waitSecondValue = false;
+    } else {
+        displayValue = displayValue === '0' ? num : displayValue + num;
+    }
+
+    console.log(displayValue, firstValue, operator, waitSecondValue);
+}
+
+function inputDecimal() {
+    if (!displayValue.includes('.'))
+        displayValue += '.';
+}
+
+function clear() {
+    displayValue = '0';
+}
